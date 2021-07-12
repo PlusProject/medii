0cde7dc5bf60fe8c49c850b4ed3fa3f0ec737405
HAKSAN <cranemont@gmail.com>
2021-07-12 23:01:27 +0900

    Init frontend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>
+      <SearchForm />
+    </div>
+    <div class="content-app">
+      <transition
+        name="fadeInUp"
+        mode="out-in"
+      >
+        <router-view />
+      </transition>
+    </div>
+  </div>
+</template>
+<script>
+import SearchForm from './SearchForm.vue'
+
+export default {
+  name: 'Home',
+  components: {
+      SearchForm
+  }
+}
+</script>
+
+<style>
+  .content-app {
+    margin:0;
+  }
+</style>
