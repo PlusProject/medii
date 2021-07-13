3752f5d5c5d782cf779f646680a522195a86c6b1
HAKSAN <cranemont@gmail.com>
2021-07-14 01:21:13 +0900

    Add search by name api(SearchName)

--- backend/search/serializers.py
+++ backend/search/serializers.py
@@ -1,12 +1,47 @@
+from django.db import models
 from rest_framework import serializers
-from .models import Doctor, Person
+from .models import ClinicalTrials, Doctor, Person, Participate, Thesis, Writes
 
 class DoctorSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Doctor
-        fields = '__all__'
+        fields = ['major', 'education', 'career', 'url']
+
 
 class PersonSerializer(serializers.ModelSerializer):
+    #doctor_info = DoctorSerializer(source='pid', read_only=True)
+
     class Meta:
         model = Person
         fields = '__all__'
+
+
+class ClinicalTrialsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ClinicalTrials
+        fields = '__all__'
+
+
+class ThesisSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Thesis
+        fields = ['tid', 'title', 'journal', 'year', 'citation', 'coworker']
+
+
+class ParticipateSerializer(serializers.ModelSerializer):
+    clinical_trials = ClinicalTrialsSerializer(source='cid', read_only=True)
+
+    class Meta:
+        model = Participate
+        fields = ['clinical_trials', 'position', 'source_name']
+
+
+class WritesSerializer(serializers.ModelSerializer):
+    thesis = ThesisSerializer(source='tid', read_only=True)
+
+    class Meta:
+        model = Writes
+        fields = ['thesis']
--- backend/search/views.py
+++ backend/search/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Doctor, Person
-from .serializers import DoctorSerializer, PersonSerializer
+from .models import ClinicalTrials, Doctor, Person, Participate, Writes
+from .serializers import ClinicalTrialsSerializer, DoctorSerializer, PersonSerializer, ParticipateSerializer, WritesSerializer
 from .serializers import serializers
 # Create your views here.
 
@@ -15,6 +15,20 @@ class DoctorTest(APIView):
 
 class SearchName(APIView):
     def get(self, request):
-        queryset = Person.objects.filter(name_kor=request.GET.get('doctor_name'))
-        serializer = PersonSerializer(queryset, many=True)
-        return Response(serializer.data)
+        personQuery = Person.objects.get(name_kor=request.GET.get('doctor_name'))
+        person = PersonSerializer(personQuery)
+
+        doctorQuery = Doctor.objects.get(pid=personQuery.pid)
+        doctor = DoctorSerializer(doctorQuery)
+
+        participateQuery = Participate.objects.filter(pid=personQuery.pid)
+        participation = ParticipateSerializer(participateQuery, many=True)
+
+        writesQuery = Writes.objects.filter(pid=personQuery.pid)
+        writes = WritesSerializer(writesQuery, many=True)
+        return Response({
+            'person': person.data,
+            'doctor_info': doctor.data,
+            'participate': participation.data,
+            'writes': writes.data
+        })
