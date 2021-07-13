635d0a85d6cad7013fc8b8290a874c179edd5563
HakSan <cranemont@gmail.com>
2021-07-13 19:01:48 +0900

    Add SearchName view

--- backend/search/views.py
+++ backend/search/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Doctor
+from .models import Doctor, Person
 from .serializers import DoctorSerializer
 from .serializers import serializers
 # Create your views here.
@@ -10,5 +10,11 @@ from .serializers import serializers
 class DoctorTest(APIView):
     def get(self, request):
         queryset = Doctor.objects.all()
+        serializer = DoctorSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class SearchName(APIView):
+    def get(self, request):
+        queryset = Person.objects.filter(name_kor=request.data['doctor_name'])
         serializer = DoctorSerializer(queryset, many=True)
         return Response(serializer.data)
