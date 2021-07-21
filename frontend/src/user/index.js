2c693e883a7032d28fe120d4c284d4b6eb3b9c53
HAKSAN <cranemont@gmail.com>
2021-07-21 11:07:40 +0900

    Add eslint & Vuetify
    eslint 상세설정필요

--- frontend/src/user/index.js
+++ frontend/src/user/index.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import vuetify from '../plugins/vuetify'
+
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
-new Vue(Vue.util.extend({ router, store }, App)).$mount('#app')
+
+new Vue(Vue.util.extend({ router, store, vuetify }, App)).$mount('#app')
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,18 +1,22 @@
 module.exports = {
-    pages: {
-        admin: {
-            entry: './src/admin/index.js',
-            template: './src/admin/index.html',
-            filename: 'index.html',
-            title: 'DB Test Page',
-            chunks: ['chunk-vendors', 'chunk-common', 'admin']
-        },
-        user: {
-            entry: './src/user/index.js',
-            template: './src/user/index.html',
-            filename: 'index.html',
-            title: 'MEDIAI+',
-            chunks: ['chunk-vendors', 'chunk-common', 'user']
-        }
-    }
+  pages: {
+      admin: {
+          entry: './src/admin/index.js',
+          template: 'src/admin/index.html',
+          filename: 'admin/index.html',
+          title: 'DB Test Page',
+          chunks: ['chunk-vendors', 'chunk-common', 'admin']
+      },
+      user: {
+          entry: './src/user/index.js',
+          template: 'src/user/index.html',
+          filename: 'index.html',
+          title: 'MEDIAI+',
+          chunks: ['chunk-vendors', 'chunk-common', 'user']
+      }
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
