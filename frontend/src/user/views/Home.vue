049957fd1b051f3a733afa83705c807f37855f6a
HAKSAN <cranemont@gmail.com>
2021-07-22 12:57:18 +0900

    Add toolbar in Home.vue

--- frontend/src/user/views/Home.vue
+++ frontend/src/user/views/Home.vue
@@ -1,8 +1,38 @@
 <template>
-  <span>test page</span>
+  <v-sheet
+    class="toolbar-sheet"
+    color="white"
+    elevation="12"
+    height="50"
+    outlined
+    rounded
+    :width="styles.sheetWidth"
+    :style="toolbarSheet"
+  ></v-sheet>
 </template>
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      styles: {
+        sheetWidth: 600
+      }
+    }
+  },
+  computed: {
+    toolbarSheet () {
+      return {
+        top: '50px',
+        left: '-webkit-calc(50% - ' + this.styles.sheetWidth/2 + 'px)' 
+      }
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .toolbar-sheet {
+    position: relative;
+  }
+</style>
