bf8f314a1c583c4d09c5e7672b474e9299a079a8
HAKSAN <cranemont@gmail.com>
2021-07-22 15:56:21 +0900

    Update style setting

--- frontend/src/user/views/Home.vue
+++ frontend/src/user/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-sheet
+  <v-toolbar
+    dense
+    floating
     class="toolbar-sheet"
     color="white"
     elevation="12"
@@ -8,23 +10,50 @@
     rounded
     :width="styles.sheetWidth"
     :style="toolbarSheet"
-  ></v-sheet>
+  >
+    <v-text-field
+      hide-details
+      append-icon="mdi-magnify"
+      single-line
+      :width="styles.sheetWidth"
+    ></v-text-field>
+
+    <v-btn icon>
+      <v-icon>mdi-crosshairs-gps</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-toolbar>
+  <!-- <v-sheet
+    class="toolbar-sheet"
+    color="white"
+    elevation="12"
+    height="50"
+    outlined
+    rounded
+    :width="styles.sheetWidth"
+    :style="toolbarSheet"
+  ></v-sheet> -->
 </template>
+
 <script>
 export default {
   name: 'Home',
   data () {
     return {
       styles: {
-        sheetWidth: 600
+        sheetWidth: 600,
+        toolbarTop: 40
       }
     }
   },
   computed: {
     toolbarSheet () {
       return {
-        top: '50px',
-        left: '-webkit-calc(50% - ' + this.styles.sheetWidth/2 + 'px)' 
+        top: this.styles.toolbarTop + 'vh'
+        // left: '-webkit-calc(50% - ' + this.styles.sheetWidth/2 + 'px)' 
       }
     }
   }
@@ -34,5 +63,8 @@ export default {
 <style lang="scss" scoped>
   .toolbar-sheet {
     position: relative;
+  }
+  .v-toolbar__content, .v-toolbar__extension {
+    width: 600px;
   }
 </style>
