b1153df06def25b4d9df970991feb6cafdee3c77
HAKSAN <cranemont@gmail.com>
2021-07-22 12:56:04 +0900

    Update main page style
    배경색 지정, 스크롤 제거

--- frontend/src/user/App.vue
+++ frontend/src/user/App.vue
@@ -1,7 +1,30 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app id="app" style="height: 100%">
+
+
+    <v-app-bar
+      app
+      color="#F3F5FF"
+      fixed
+      >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <!-- <v-footer app>
+
+    </v-footer> -->
+  </v-app>
 </template>
 
 <script>
@@ -18,6 +41,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #F3F5FF;
 }
+/* div > .v-application--wrap {
+  min-height: 908px;
+} */
 </style>
