c0a71b9c3a5b64abf0f616e10f9e403867dd221d
LeeGyuRi <lj01081512@gmail.com>
2021-12-13 20:30:40 +0900

    add recommend page

--- frontend/src/user/views/index.js
+++ frontend/src/user/views/index.js
@@ -2,7 +2,8 @@ import Home from './Home.vue'
 import DoctorList from './DoctorList'
 import ClinicalTrialsList from './ClinicalTrialsList'
 import Page4 from './Page4'
+import Recommend from './Recommend'
 
 export {
-  Home, DoctorList, ClinicalTrialsList, Page4
+  Home, DoctorList, ClinicalTrialsList, Page4, Recommend
 }
